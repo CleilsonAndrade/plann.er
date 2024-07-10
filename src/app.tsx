099dec9149a1d57c4ae408a78942ec8f79c9424c
CleilsonAndrade <cleilsonjose@hotmail.com>
2024-07-10 00:07:39 -0300

    ✨ feat(modal): function to remove email list

--- src/app.tsx
+++ src/app.tsx
@@ -46,6 +46,12 @@ export function App() {
     event.currentTarget.reset()
   }
 
+  function removeEmailFromInvite(emailToRemove: string) {
+    const newEmailList = emailsToInvite.filter(email => email !== emailToRemove)
+
+    setEmailsToInvite(newEmailList)
+  }
+
   return (
     <div className="h-screen flex items-center justify-center bg-pattern bg-no-repeat bg-center">
       <div className="max-w-3xl w-full px-6 text-center space-y-10">
@@ -128,7 +134,7 @@ export function App() {
                   return (
                     <div key={email} className="py-1.5 px-2.5 rounded-md bg-zinc-800 flex items-center gap-2">
                       <span className="text-zinc-300">{email}</span>
-                      <button type="button">
+                      <button type="button" onClick={() => removeEmailFromInvite(email)}>
                         <X className="size-4 text-zinc-400" />
                       </button>
                     </div>
