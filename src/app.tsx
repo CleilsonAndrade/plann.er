931aa0123e5d416b395cb1985e353044df1a5c4f
CleilsonAndrade <cleilsonjose@hotmail.com>
2024-07-10 15:01:48 -0300

    ✨ feat(home): display number of people invited

--- src/app.tsx
+++ src/app.tsx
@@ -100,8 +100,15 @@ export function App() {
               <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center shadow-shape gap-3">
                 <button type="button" onClick={openGuestsModal} className="flex items-center gap-2 flex-1 text-left">
                   <UserRoundPlus className="size-5 text-zinc-400" />
-                  <span className="text-zinc-400 text-lg flex-1s">Quem estará na viagem?</span>
-
+                  {
+                    emailsToInvite.length > 0 ? (
+                      <span className="text-zinc-100">
+                        {emailsToInvite.length} pessoa(s) convidada(s)
+                      </span>
+                    ) : (
+                      <span className="text-zinc-400 text-lg flex-1s">Quem estará na viagem?</span>
+                    )
+                  }
                 </button>
 
                 <div className="w-px h-6 bg-zinc-800" />
