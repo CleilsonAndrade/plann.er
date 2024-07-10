eae5952b006cb3dc38e9477af4a23364c75548fb
CleilsonAndrade <cleilsonjose@hotmail.com>
2024-07-09 23:13:47 -0300

    ✨ feat(home): added state to use inputs

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,16 @@
-import { ArrowRight, Calendar, MapPin } from "lucide-react"
+import { ArrowRight, Calendar, MapPin, Settings2, UserRoundPlus } from "lucide-react"
+import { useState } from "react"
 
 export function App() {
+  const [isGuestInputOpen, setIsGuestInputOpen] = useState(false)
+
+  function openGuestInput() {
+    setIsGuestInputOpen(true)
+  }
+
+  function closeGuestInput() {
+    setIsGuestInputOpen(false)
+  }
 
   return (
     <div className="h-screen flex items-center justify-center bg-pattern bg-no-repeat bg-center">
@@ -10,23 +20,51 @@ export function App() {
         </div>
 
         <p className="text-zinc-300 text-lg">Convide seus amigos e planeje sua proxima viagem!</p>
-        <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center shadow-shape gap-3">
-          <div className="flex items-center gap-2 flex-1">
-            <MapPin className="size-5 text-zinc-400" />
-            <input type="text" placeholder="Para onde voce vai?" className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1" />
+        <div className="space-y-4">
+          <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center shadow-shape gap-3">
+            <div className="flex items-center gap-2 flex-1">
+              <MapPin className="size-5 text-zinc-400" />
+              <input disabled={isGuestInputOpen} type="text" placeholder="Para onde voce vai?" className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1" />
+            </div>
+
+            <div className="flex items-center gap-2">
+              <Calendar className="size-5 text-zinc-400" />
+              <input disabled={isGuestInputOpen} type="text" placeholder="Quando?" className="bg-transparent text-lg placeholder-zinc-400 w-40 outline-none" />
+            </div>
+
+            <div className="w-px h-6 bg-zinc-800" />
+
+            {
+              isGuestInputOpen ? (
+                <button onClick={closeGuestInput} className="bg-zinc-800 text-zinc-200 rounded-lg px-4 py-2 font-medium flex items-center gap-2 hover:bg-zinc-700">
+                  Alterar local/data
+                  <Settings2 className="size-5" />
+                </button>
+              ) : (
+                <button onClick={openGuestInput} className="bg-lime-300 text-lime-950 rounded-lg px-4 py-2 font-medium flex items-center gap-2 hover:bg-lime-400">
+                  Continuar
+                  <ArrowRight className="size-5" />
+                </button>
+              )}
           </div>
 
-          <div className="flex items-center gap-2">
-            <Calendar className="size-5 text-zinc-400" />
-            <input type="text" placeholder="Quando?" className="bg-transparent text-lg placeholder-zinc-400 w-40 outline-none" />
-          </div>
+          {
+            isGuestInputOpen && (
+              <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center shadow-shape gap-3">
+                <div className="flex items-center gap-2 flex-1">
+                  <UserRoundPlus className="size-5 text-zinc-400" />
+                  <input type="text" placeholder="Quem estará na viagem?" className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1" />
+                </div>
 
-          <div className="w-px h-6 bg-zinc-800" />
+                <div className="w-px h-6 bg-zinc-800" />
 
-          <button className="bg-lime-300 text-lime-950 rounded-lg px-4 py-2 font-medium flex items-center gap-2 hover:bg-lime-400">
-            Continuar
-            <ArrowRight className="size-5" />
-          </button>
+                <button className="bg-lime-300 text-lime-950 rounded-lg px-4 py-2 font-medium flex items-center gap-2 hover:bg-lime-400">
+                  Confirmar viagem
+                  <ArrowRight className="size-5" />
+                </button>
+              </div>
+            )
+          }
         </div>
 
         <p className="text-small text-zinc-500">
