a0e7dc7ce6f1255607a6a4daafa8995901194abb
CleilsonAndrade <cleilsonjose@hotmail.com>
2024-07-12 18:41:36 -0300

    🐞 fix(env): removed dotenv

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,5 @@
 import axios from "axios";
-// import dotenv from 'dotenv';
-// dotenv.config();
 
 export const api = axios.create({
-  baseURL: "http://localhost:3333"
-})
+  baseURL: 'http://localhost:3333',
+});
