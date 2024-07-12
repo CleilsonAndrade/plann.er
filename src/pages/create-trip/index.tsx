8fe6657a71de013d33b97fca8a91b0f6a4d43090
CleilsonAndrade <cleilsonjose@hotmail.com>
2024-07-11 22:08:22 -0300

    🐞 fix(api): added suport api and creation trip

--- src/pages/create-trip/index.tsx
+++ src/pages/create-trip/index.tsx
@@ -1,6 +1,7 @@
 import { FormEvent, useState } from "react"
 import { DateRange } from "react-day-picker"
 import { useNavigate } from "react-router-dom"
+import { api } from "../../lib/axios"
 import { ConfirmTripModal } from "./confirm-trip-modal"
 import { InviteGuestsModal } from "./invite-guests-modal"
 import { DestinationAndDateStep } from "./steps/destination-and-date-step"
@@ -70,9 +71,37 @@ export function CreateTripPage() {
     setEmailsToInvite(newEmailList)
   }
 
-  function createTrip(event: FormEvent<HTMLFormElement>) {
+  async function createTrip(event: FormEvent<HTMLFormElement>) {
     event.preventDefault()
-    navigate('/trips/123')
+
+    if (!destination) {
+      return
+    }
+
+    if (!eventStartAndEndDates?.from || !eventStartAndEndDates.to) {
+      return
+    }
+
+    if (emailsToInvite.length === 0) {
+      return
+    }
+
+    if (!ownerEmail || !ownerName) {
+      return
+    }
+
+    const response = await api.post('trips', {
+      destination: destination,
+      starts_at: eventStartAndEndDates.from,
+      ends_at: eventStartAndEndDates.to,
+      emails_to_invite: emailsToInvite,
+      owner_name: ownerName,
+      owner_email: ownerEmail,
+    })
+
+    const { tripId } = response.data
+
+    navigate(`/trips/${tripId}`)
   }
 
   return (
