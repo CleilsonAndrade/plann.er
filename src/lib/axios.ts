35b0ee88094d41dcc910ef70c5210543417cdf31
CleilsonAndrade <cleilsonjose@hotmail.com>
2024-07-11 23:26:23 -0300

    🔎 ref(api): switched api link to use env

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,11 +1,7 @@
 import axios from "axios";
-// import dotenv from 'dotenv';
-// dotenv.config();
-
-// export const api = axios.create({
-//   baseURL: process.env.API_BASE_URL
-// })
+import dotenv from 'dotenv';
+dotenv.config();
 
 export const api = axios.create({
-  baseURL: 'http://localhost:3333'
+  baseURL: process.env.API_BASE_URL
 })
